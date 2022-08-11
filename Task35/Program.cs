c3018f6ae5eb82b4aaecbe9ee775367f02886395
Andrey_Demidov <a_demidov91@mail.ru>
2022-08-11 12:35:12 +0300

    Seminar5 tasks

---
+++ Task35/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] FillArray(int size, int leftRange, int rightRange){
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++){
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+int[] array = FillArray(123, 0, 1000);
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+
+int count = 0;
+for (int i=0; i<array.Length; i++){
+    if (array[i]>=10 && array[i]<=99){
+        count = count +1;
+    }
+}
+Console.Write($"количество элементов массива, значения которых лежат в отрезке [10,99] = {count}");
---
+++ Task38/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите массив вещественных чисел: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[size];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Введите значение элемента массива: ");
+    array[i] = Convert.ToDouble(Console.ReadLine());
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+double max = array[0];
+double min = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+Console.WriteLine($"Разница между максимальным и минимальным элеменатми равна: {max - min}");
